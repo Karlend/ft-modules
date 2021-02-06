9818c5692592bb3ce486568ffa468b4e00ef6bb5
Johnny <wreckerplayer@gmail.com>
2021-02-06 13:12:50 +0200

    Update lesya.py

--- lesya.py
+++ lesya.py
@@ -5,6 +5,7 @@ from telethon import types
 lesya = 757724042  # ID бота
 init = False
 next_bonus = 0
+stats = {}
 
 @loader.tds
 class AutoLesyaMod(loader.Module):
@@ -13,14 +14,10 @@ class AutoLesyaMod(loader.Module):
 
 	async def client_ready(self, client, db):
 		self._me = await client.get_me()
-		await client.send_message(lesya, "Профиль")
 
 	async def watcher(self, message):
 		global init
 		global next_bonus
-		if init:
-			message.client.send_message(lesya, "Профиль 2")
-			init = True
 		if not isinstance(message, types.Message):
 			return
 		chat_id = utils.get_chat_id(message)
@@ -30,10 +27,18 @@ class AutoLesyaMod(loader.Module):
 		if not text:
 			return
 		now = time.time()
+		if (text.find("Ваш профиль:") != -1): # Инфа по профилю привет
+			global stats
+			stats.premium = text.find("Статус: Premium") != 1
+			stats.vip = (text.find("Статус: Premium") != 1) or (text.find("Статус: VIP") != 1)
+			stats.work = text.find("Работа: ") != 1
+			stats.clan = text.find("Клан: ") != 1
 		if (text.find("Ваши питомцы проиграли") != -1) or (text.find("Ваши питомцы победили") != -1): # Продолжение боя
 			await utils.answer(message, "Бой") # todo: чек времени, когда нету стероидов
-		if (now > next_bonus) and (next_bonus > 0):
+		if (now > next_bonus):
 			await message.client.send_message(lesya, "Бонус")
-			await message.client.send_message(lesya, "Вип бонус")
-			await message.client.send_message(lesya, "Премиум бонус")
+			if stats.vip:
+				await message.client.send_message(lesya, "Вип бонус")
+			if stats.premium
+				await message.client.send_message(lesya, "Премиум бонус")
 			next_bonus = now + 60 * 60 * 8
