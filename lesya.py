ac378dd88e2968d0323fa2ee14acc6574fab9135
Johnny <wreckerplayer@gmail.com>
2021-02-06 13:40:52 +0200

    Update lesya.py

--- lesya.py
+++ lesya.py
@@ -4,7 +4,10 @@ from telethon import types
 
 lesya = 757724042  # ID бота
 init = False
-next_bonus = 0
+times = {
+	"bonus": 0,
+	"work": 0
+}
 stats = {}
 
 @loader.tds
@@ -17,7 +20,7 @@ class AutoLesyaMod(loader.Module):
 
 	async def watcher(self, message):
 		global init
-		global next_bonus
+		global times
 		if not isinstance(message, types.Message):
 			return
 		chat_id = utils.get_chat_id(message)
@@ -36,10 +39,14 @@ class AutoLesyaMod(loader.Module):
 			stats["clan"] = text.find("Клан: ") != 1
 		if (text.find("Ваши питомцы проиграли") != -1) or (text.find("Ваши питомцы победили") != -1): # Продолжение боя
 			await utils.answer(message, "Бой") # todo: чек времени, когда нету стероидов
-		if (now > next_bonus) and stats.get("has"): # todo: Получение времени для следующего бонуса из сообщения
-			next_bonus = now + 60 * 60 * 8
+		if stats.get("has") and (now > times.get("bonus")): # todo: Получение времени для следующего бонуса из сообщения
+			times["bonus"] = now + 60 * 60 * 8
 			await message.client.send_message(lesya, "Бонус")
 			if stats.get("vip"):
 				await message.client.send_message(lesya, "Вип бонус")
 			if stats.get("premium"):
 				await message.client.send_message(lesya, "Премиум бонус")
+		if stats.get("work") and (now > times.get("work")): # todo: Получение времени для следующей работы
+			times["work"] = now + 60 * 13 # раз в 13 минут
+			for i in range(3):
+				await message.client.send_message(lesya, "Работать")
