b184fced2e076163575de356329a04f1cc5725db
Johnny <wreckerplayer@gmail.com>
2021-02-06 12:27:03 +0200

    Update lesya.py

--- lesya.py
+++ lesya.py
@@ -3,17 +3,24 @@ import time # time.time() для времени. Используется для
 from telethon import types
 
 lesya = 757724042  # ID бота
+init = False
+next_bonus = 0
+
 @loader.tds
 class AutoLesyaMod(loader.Module):
 	"""Автоматизация LesyaBot"""
 	strings = {"name": "LesyaBot"}
-	next_bonus = 0
 
 	async def client_ready(self, client, db):
 		self._me = await client.get_me()
 		client.send_message(lesya, "Профиль")
 
 	async def watcher(self, message):
+		global init
+		global next_bonus
+		if init:
+			await message.client.send_message(lesya, "Профиль")
+			init = True
 		if not isinstance(message, types.Message):
 			return
 		chat_id = utils.get_chat_id(message)
@@ -25,7 +32,7 @@ class AutoLesyaMod(loader.Module):
 		now = time.time()
 		if (text.find("Ваши питомцы проиграли") != -1) or (text.find("Ваши питомцы победили") != -1): # Продолжение боя
 			await utils.answer(message, "Бой") # todo: чек времени, когда нету стероидов
-		if False: #next_bonus and (now > next_bonus) and (next_bonus > 0):
+		if (now > next_bonus) and (next_bonus > 0):
 			await message.client.send_message(lesya, "Бонус")
 			await message.client.send_message(lesya, "Вип бонус")
 			await message.client.send_message(lesya, "Премиум бонус")
