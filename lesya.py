8ee1ed13924d6fbada1e5384661c759930552294
Johnny <wreckerplayer@gmail.com>
2021-02-06 13:24:37 +0200

    Update lesya.py

--- lesya.py
+++ lesya.py
@@ -29,16 +29,16 @@ class AutoLesyaMod(loader.Module):
 		now = time.time()
 		if (text.find("Ваш профиль:") != -1): # Инфа по профилю привет
 			global stats
-			stats.premium = text.find("Статус: Premium") != 1
-			stats.vip = (text.find("Статус: Premium") != 1) or (text.find("Статус: VIP") != 1)
-			stats.work = text.find("Работа: ") != 1
-			stats.clan = text.find("Клан: ") != 1
+			stats["premium"] = text.find("Статус: Premium") != 1
+			stats["vip"] = (text.find("Статус: Premium") != 1) or (text.find("Статус: VIP") != 1)
+			stats["work"] = text.find("Работа: ") != 1
+			stats["clan"] = text.find("Клан: ") != 1
 		if (text.find("Ваши питомцы проиграли") != -1) or (text.find("Ваши питомцы победили") != -1): # Продолжение боя
 			await utils.answer(message, "Бой") # todo: чек времени, когда нету стероидов
 		if (now > next_bonus):
 			next_bonus = now + 60 * 60 * 8
 			await message.client.send_message(lesya, "Бонус")
-			if stats.vip:
+			if stats["vip"]:
 				await message.client.send_message(lesya, "Вип бонус")
-			if stats.premium:
+			if stats["premium"]:
 				await message.client.send_message(lesya, "Премиум бонус")
